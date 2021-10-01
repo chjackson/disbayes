0474dcb578b9309ba6719700ab213939a4a902c7
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2021-10-01 18:38:15 +0100

    Missing file restored

---
+++ tests/testthat/data.r
@@ -0,0 +1,10 @@
+library(dplyr)
+
+ihdbristol <- ihdengland %>% 
+  dplyr::filter(area=="Bristol", gender=="Male") %>%
+  mutate(mort_prob = mort_num/mort_denom,
+       mort_lower = plogis(qlogis(mort_prob)-1),
+       mort_upper = plogis(qlogis(mort_prob)+1))
+
+ihdmale <- ihdengland %>% 
+  dplyr::filter(gender=="Male")
