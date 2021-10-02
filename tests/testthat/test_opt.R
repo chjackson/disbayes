9e6d5dbd4edc4c4664335899601cb267e3e12dda
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2021-10-02 18:13:55 +0100

    Added CHECK workflow

--- tests/testthat/test_opt.R
+++ tests/testthat/test_opt.R
@@ -1,12 +1,13 @@
 source("data.r")
 
 test_that("standard disbayes model, optimisation",{
+  set.seed(1)
   db <- disbayes(dat = ihdbristol,
                     inc_num = "inc_num", inc_denom = "inc_denom",
                     prev_num = "prev_num", prev_denom = "prev_denom",
                     mort_num = "mort_num", mort_denom = "mort_denom",
-                    eqage = 40, loo=FALSE)
+                    eqage = 40, loo=FALSE, seed=1)
   res <- tidy(db) %>% filter(var=="cf", age==60)
   expect_equal(res$mode, 0.0172128687091707)
-  expect_equal(res$`50%`, 0.0170881063099402)
+  expect_equal(res$`50%`, 0.0173084702459764)
 })
