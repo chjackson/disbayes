c813b06f0fa8a8c7f63e8fc5f7b534a814f001b5
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2021-10-24 15:33:29 +0100

    cfnaive removed, since we now have a more accurate fast estimator

--- tests/testthat/data.r
+++ tests/testthat/data.r
@@ -5,7 +5,9 @@ ihdbristol <- ihdengland %>%
   dplyr::filter(area=="Bristol", gender=="Male") %>%
   mutate(mort_prob = mort_num/mort_denom,
        mort_lower = plogis(qlogis(mort_prob)-1),
-       mort_upper = plogis(qlogis(mort_prob)+1))
+       mort_upper = plogis(qlogis(mort_prob)+1),
+       rem_num = 10,
+       rem_denom = 100)
 
 ihdmale <- ihdengland %>% 
   dplyr::filter(gender=="Male")
